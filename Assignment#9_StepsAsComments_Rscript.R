d1183586557eab3de2d0a1ac9701dca285617028
lmonserrate <51416553+lmonserrate@users.noreply.github.com>
2020-07-20 01:13:45 -0400

    Week 8 - Assignment

---
+++ Assignment#9_StepsAsComments_Rscript.R
@@ -0,0 +1,37 @@
+##library(tidyverse)
+##VT_Height_data <- read_csv("~/GitHub/ag-problems-semester/VarietyTrial1_summer_2019_PlantHeight.csv")
+##FlowerInduc_data <- read_csv("~/GitHub/ag-problems-semester/Flowering induction per variety_2019.csv")
+##PlotVariety <- read_csv("~/GitHub/ag-problems-semester/PlotVarietyFile.csv")
+##VT_Height_data <- merge(VT_Height_data, PlotVariety, by=c("Column", "Row", "Block"))
+##VT_HeightInduc_data <- merge(VT_Height_data, FlowerInduc_data, by=c("Variety"))
+##library(lubridate)
+##VT_HeightInduc_data_by_date <- VT_HeightInduc_data %>%
+  ##mutate(Date = make_date(Year, Month, Day))
+##VT_FinalHeightInduc_data_by_date <- VT_HeightInduc_data_by_date %>%
+  ##filter(Height_cm > 1)%>%
+  ##select(Height_cm, Variety, Date, induc_date) %>%
+  ##filter((Date == "2019-07-17" & Variety == "Tygra")|
+        ##(Date == "2019-07-24" & Variety %in% c("CFX-1", "CFX-2", "CRS-1", "Canda", "Joey"))|
+        ##(Date == "2019-08-09" & Variety %in% c("CFX-1", "HanFNQ", "HanFN-H", "Helena"))|
+        ##(Date == "2019-08-20"))
+##resultsVT_HeightsPerVar <- aov(Height_cm ~ Variety, data=VT_FinalHeightInduc_data_by_date)
+##summary(resultsVT_HeightsPerVar)
+##resultsVT_HeightsPerInduc <- aov(Height_cm ~ induc_date, data=VT_FinalHeightInduc_data_by_date)
+##summary(resultsVT_HeightsPerInduc)
+
+##PP1_Height_data <- read_csv("~/GitHub/ag-problems-semester/PilotPlot1_summer_2019_PlantHeight-SexRatio.csv")
+##PP1_Height_data <- merge(PP1_Height_data, PlotVariety, by=c("Column", "Row", "Block"))
+##PP1_HeightInduc_data <- merge(PP1_Height_data, FlowerInduc_data, by=c("Variety"))
+##PP1_HeightInduc_data_by_date <- PP1_HeightInduc_data %>%
+  ##mutate(Date = make_date(Year, Month, Day))
+##PP1_FinalHeightInduc_data_by_date <- PP1_HeightInduc_data_by_date %>%
+  ##filter(Date == "2019-07-19" | Date == "2019-07-24" | Date == "2019-08-20")%>%
+  ##filter(Height_cm > 1)%>%
+  ##select(Height_cm, Variety, Date, induc_date)%>%
+  ##filter((Date == "2019-07-19" & Variety == "Tygra")|
+        ##(Date == "2019-07-24" & Variety %in% c("CarmagnolaSelezionata", "Eletta"))|
+        ##(Date == "2019-08-20"))
+##resultsPP1 <- aov(Height_cm ~ Variety, data=PP1_FinalHeightInduc_data_by_date)
+##summary(resultsPP1)
+##resultsPP1_HeightsPerInduc <- aov(Height_cm ~ induc_date, data=PP1_FinalHeightInduc_data_by_date)
+##summary(resultsVT_HeightsPerInduc)
